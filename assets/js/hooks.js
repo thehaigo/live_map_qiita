53f053a3ea2eff5eb676571a1eee001b8a320f95
thehaigo <sirannha@gmail.com>
2021-12-14 21:54:47 +0900

    impl google map js hook

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -1,4 +1,5 @@
 import { Loader } from "@googlemaps/js-api-loader";
+
 let Hooks = {}
 const apiKey = "your api key"
 Hooks.Map = {
@@ -6,6 +7,7 @@ Hooks.Map = {
     const loader = new Loader({
       apiKey: apiKey,
       version: "weekly",
+      libraries: ["places"]
     })
 
     loader.load().then(() => {
@@ -16,8 +18,36 @@ Hooks.Map = {
           zoom: 9
         }
       );
+      const service = new google.maps.places.PlacesService(map);
       window.map = map;
+      window.service = service
+    })
 
+    this.handleEvent("search", ({keyword}) => {
+      const that = this
+      const request = {
+        query: keyword
+      };
+      window.service.textSearch(request, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          let result = results.map((r) => { 
+            return {
+              name: r.name,
+              address: r.formatted_address.split(/\s/).pop(),
+              location: r.geometry.location,
+              icon: r.icon,
+              place_id: r.place_id
+            };
+          })
+          that.pushEvent("search_result" , {result: result});
+        }
+      });
+    })
+
+    this.handleEvent("add_marker", ({location}) => {
+      let marker = new google.maps.Marker({position: location})
+      marker.setMap(window.map)
+      window.map.setCenter(location)
     })
   }
 }
--- lib/live_map_web/live/page_live.ex
+++ lib/live_map_web/live/page_live.ex
@@ -1,10 +1,37 @@
 defmodule LiveMapWeb.PageLive do
   use LiveMapWeb, :live_view
-
+  alias Phoenix.LiveView.JS
   def mount(_params, _session, socket) do
     {
       :ok,
       socket
+      |> assign(:text, "")
+      |> assign(:search_result, [])
+    }
+  end
+
+  def handle_event("search", %{"keyword" => keyword}, socket) do
+    {
+      :noreply,
+      socket
+      |> assign(:text, keyword)
+      |> push_event("search", %{keyword: keyword})
+    }
+  end
+
+  def handle_event("search_result", %{"result" => result}, socket) do
+    {
+      :noreply,
+      socket
+      |> assign(:search_result, result)
+    }
+  end
+
+  def handle_event("add_marker", location, socket) do
+    {
+      :noreply,
+      socket
+      |> push_event("add_marker", location)
     }
   end
 end
